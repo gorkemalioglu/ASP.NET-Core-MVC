a2e6b4ad28977ee9bca1cb78884d435bc97ebf70
gorkemalioglu <126862461+gorkemalioglu@users.noreply.github.com>
2025-02-24 17:10:41 +0300

    Add files via upload

---
+++ Core_Dersleri/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core_Dersleri.Models;
+
+namespace Core_Dersleri.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+        public int Index3()
+        {
+            return 24;
+        }
+        public string Index4()
+        {
+            return "merhaba  bugün hava çok soğuk";
+
+        }
+        public IActionResult Index5()
+        {
+            var ktp = new List<Kitap>()
+            {
+                new Kitap(){ID=1,KitapAd="80 Günde Devri Alem",Yazar="Jules Verne"},
+                new Kitap(){ID=2,KitapAd="Bilinmeyen Bir Kadının Mektubu",Yazar="Zweig"},
+                new Kitap(){ID=3,KitapAd="Bir Noel Hikayesi",Yazar="Charles Dickens"},
+                new Kitap(){ID=4,KitapAd="şeker portakalı",Yazar="vasconceulos"},
+
+            };
+            return View(ktp);
+        }
+        public IActionResult Index6()
+        {
+            ViewBag.deger1 = "Merhada dersler devam ediyor";
+            ViewBag.deger2 = "naber";
+            return View();
+        }
+        public IActionResult Index7()
+        {
+            return View();
+        }
+        public IActionResult Index8()
+        {
+            return View();
+        }
+        public IActionResult Index9()
+        {
+
+            return View();
+        }
+            
+
+
+    }
+}
---
+++ Core_Dersleri/Program.cs
@@ -0,0 +1,24 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseStaticFiles();
+app.UseRouting(); 
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
